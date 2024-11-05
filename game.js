459a68d574b8411b6cfc9c2ca90c72d1f83b353d
rilfreecs <156951544+rilfreecs@users.noreply.github.com>
2024-11-05 23:51:30 +0700

    Update game.js

--- game.js
+++ game.js
@@ -20,16 +20,18 @@ const game = new Phaser.Game(config);
 
 let towers = [];
 let enemies = [];
+let bullets = [];
 let enemySpawnTime = 2000; // Waktu spwan musuh dalam milidetik
 let enemySpeed = 100; // Kecepatan musuh
 
 function preload() {
-    // Load assets di sini (sprite, gambar, dll.)
+    // Load assets di sini jika diperlukan
 }
 
 function create() {
     this.input.on('pointerdown', placeTower, this); // Tempatkan menara saat klik
     this.time.addEvent({ delay: enemySpawnTime, callback: addEnemy, callbackScope: this, loop: true }); // Tambahkan musuh secara berkala
+    this.time.addEvent({ delay: 1000, callback: shootBullets, callbackScope: this, loop: true }); // Menembakkan peluru secara berkala
 }
 
 function update() {
@@ -39,6 +41,23 @@ function update() {
         if (enemy.y > this.cameras.main.height) {
             enemy.destroy(); // Hapus musuh jika keluar dari layar
         }
+    });
+
+    bullets.forEach(bullet => {
+        bullet.y -= 300 * (1 / 60); // Pergerakan peluru ke atas
+        if (bullet.y < 0) {
+            bullet.destroy(); // Hapus peluru jika keluar dari layar
+        }
+    });
+
+    // Deteksi tabrakan antara peluru dan musuh
+    bullets.forEach(bullet => {
+        enemies.forEach(enemy => {
+            if (Phaser.Geom.Intersects.CircleToRectangle(enemy.getBounds(), bullet.getBounds())) {
+                bullet.destroy(); // Hapus peluru saat mengenai musuh
+                enemy.destroy(); // Hapus musuh saat terkena peluru
+            }
+        });
     });
 }
 
@@ -50,4 +69,11 @@ function placeTower(pointer) {
 function addEnemy() {
     const enemy = this.add.circle(Math.random() * this.cameras.main.width, 0, 15, 0xff0000); // Menggambar musuh
     enemies.push(enemy);
+}
+
+function shootBullets() {
+    towers.forEach(tower => {
+        const bullet = this.add.rectangle(tower.x + 25, tower.y, 5, 10, 0x0000ff); // Menggambar peluru
+        bullets.push(bullet);
+    });
 }
